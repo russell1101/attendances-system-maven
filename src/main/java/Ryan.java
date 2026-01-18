363ad26afaa03fd59c4b0d4351b174be576fee95
Ryan <az855616@gmail.com>
2026-01-18 15:57:31 +0800

    新增 IntelliJ IDEA 設定檔 (.idea)

--- src/main/java/Ryan.java
+++ src/main/java/Ryan.java
@@ -1,5 +1,5 @@
-public class Ryan public static void main(String[] args) {
-    System.out.println("痾密陀佛");
+public class Ryan {public static void main(String[] args) {
+    System.out.println("佛祖在看著你");
 }
 }
-      
+
