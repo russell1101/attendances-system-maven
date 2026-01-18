da11aee77008ccc96ee5fd150fe1560e403d5b9f
William Lee <ithan0117@gmail.com>
2026-01-18 15:29:10 +0800

    uuu

--- src/main/java/William.java
+++ src/main/java/William.java
@@ -3,6 +3,6 @@ public class William {
 
 	public static void main(String[] args) {
 		System.out.println("str1 == str2，罰款1000元");
-		System.out.println("上傳編譯錯誤的程式，罰款1000元，鞭刑2下");
+		System.out.println("上傳編譯錯誤的程式，罰款1000元，鞭刑5下");
 	}
 }
